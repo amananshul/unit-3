46da6520162f2daa653839eba4898c8e68ca1f03
amananshul <80920301+amananshul@users.noreply.github.com>
2022-03-05 20:41:11 +0530

    done

--- day3 class/day3_720773/index.js
+++ day3 class/day3_720773/index.js
@@ -1,5 +1,5 @@
 // scope means access to a variable within a program. We have different scopes since we have functions in a program.
-var scope=[]
+// var scope=[]
 var scopeObj={
 p1:"Scope means access to a variable within a program. We have different scopes since we have functions in a program.",
 p2:"Variables declared within a function has the child scope.",
@@ -7,10 +7,10 @@ p3:"Variables declared outside a function has the global scope.",
 p4:"A variable within a function is not accessible from another function",
 p5:"If a variable declared within a function & outside the function, they are treated as separate variables"
 }
-scope.push(scopeObj)
-localStorage.setItem("scope" ,JSON.stringify(scope))
-console.log(scopeObj)
-var hoisting=[]
+// scope.push(scopeObj)
+localStorage.setItem("scope" ,JSON.stringify(scopeObj))
+// console.log(scopeObj)
+// var hoisting=[]
 var hoistingObj={
     p1:"JavaScript Hoisting refers to the process whereby the interpreter appears to move the declaration of functions, variables or classes to the top of their scope, prior to execution of the code.",
     p2:"Hoisting works with variables too, so you can use a variable in code before it is declared and/or initialized.",
@@ -18,9 +18,9 @@ var hoistingObj={
     p4:"Classes defined using a class declaration are hoisted, which means that JavaScript has a reference to the class.",
     p5:"During the creation phase, the JavaScript engine moves the variable and function declarations to the top of your code. This feature is known as hoisting in JavaScript."
 }
-hoisting.push(hoistingObj)
-localStorage.setItem("hoisting" ,JSON.stringify(hoisting))
-var constructor=[]
+// hoisting.push(hoistingObj)
+localStorage.setItem("hoisting" ,JSON.stringify(hoistingObj))
+// var constructor=[]
 var constructorObj={
     p1:"In a constructor function this does not have a value. It is a substitute for the new object.",
     p2:" The value of this will become the new object when a new object is created.",
@@ -28,9 +28,9 @@ var constructorObj={
     p4:"You cannot add a new property to an object constructor the same way you add a new property to an existing object",
     p5:"To add a new property to a constructor, you must add it to the constructor function:"
 }
-constructor.push(constructorObj)
-localStorage.setItem("constructor" ,JSON.stringify(constructor))
-var prototype =[];
+// constructor.push(constructorObj)
+localStorage.setItem("constructor" ,JSON.stringify(constructorObj))
+// var prototype =[];
 var prototypeObj={
     p1:"if we want to add new properties at later stage to a function which will be shared across all the instances is prototype",
     p2:"The prototype is an object that is associated with every functions and objects by default in JavaScript",
@@ -38,32 +38,28 @@ var prototypeObj={
     p4:"Every object which is created using literal syntax or constructor syntax with the new keyword, includes __proto__ property ",
     p5:"You can debug and see object's or function's prototype property in chrome or firefox's developer tool. "
 }
-prototype.push(prototypeObj)
-console.log(prototype)
-localStorage.setItem("prototype" ,JSON.stringify(prototype))
+// prototype.push(prototypeObj)
+// console.log(prototype)
+localStorage.setItem("prototype" ,JSON.stringify(prototypeObj))
 
 var scopeval=JSON.parse(localStorage.getItem("scope"))
 document.querySelector("#scope").addEventListener("click", myScope)
 function myScope(){
         document.querySelector("#container").innerText=null
 
-    scopeval.map(function(elem,index){
         var div=document.createElement("ul")
-        var p1 = document.createElement("li")
-        p1.innerText=elem.p1
-        var p2 = document.createElement("li")
-        p2.innerText=elem.p2
-        var p3 = document.createElement("li")
-        p3.innerText=elem.p3
-        var p4 = document.createElement("li")
-        p4.innerText=elem.p4
-        var p5 = document.createElement("li")
-        p5.innerText=elem.p5
-       
-        div.append(p1,p2,p3,p4,p5)
+        for(var key in scopeval)
+        {
+
+            var p1 = document.createElement("li")
+            p1.innerText= scopeval[key]
+            
+            
+            div.append(p1)
+        }
         document.querySelector("#container").append(div)
         
-    })
+   
 
 
 }
@@ -73,24 +69,18 @@ document.querySelector("#prototype").addEventListener("click", myprototype)
 function myprototype(){
         document.querySelector("#container").innerText=null
 
-    document.querySelector("#container").innerText=null
-    prototypeval.map(function(elem,index){
         var div=document.createElement("ul")
-        var p1 = document.createElement("li")
-        p1.innerText=elem.p1
-        var p2 = document.createElement("li")
-        p2.innerText=elem.p2
-        var p3 = document.createElement("li")
-        p3.innerText=elem.p3
-        var p4 = document.createElement("li")
-        p4.innerText=elem.p4
-        var p5 = document.createElement("li")
-        p5.innerText=elem.p5
-       
-        div.append(p1,p2,p3,p4,p5)
+        for(var key in prototypeval)
+        {
+
+            var p1 = document.createElement("li")
+            p1.innerText= prototypeval[key]
+            
+            
+            div.append(p1)
+        }
         document.querySelector("#container").append(div)
         
-    })
 
 
 }
@@ -99,24 +89,17 @@ var constructorval=JSON.parse(localStorage.getItem("constructor"))
 document.querySelector("#constructor").addEventListener("click", myconstructor)
 function myconstructor(){
         document.querySelector("#container").innerText=null
-
-    constructorval.map(function(elem,index){
         var div=document.createElement("ul")
-        var p1 = document.createElement("li")
-        p1.innerText=elem.p1
-        var p2 = document.createElement("li")
-        p2.innerText=elem.p2
-        var p3 = document.createElement("li")
-        p3.innerText=elem.p3
-        var p4 = document.createElement("li")
-        p4.innerText=elem.p4
-        var p5 = document.createElement("li")
-        p5.innerText=elem.p5
-       
-        div.append(p1,p2,p3,p4,p5)
+        for(var key in constructorval)
+        {
+
+            var p1 = document.createElement("li")
+            p1.innerText= constructorval[key]
+            
+            
+            div.append(p1)
+        }
         document.querySelector("#container").append(div)
-        
-    })
 
 
 }
@@ -125,24 +108,17 @@ var hoistingval=JSON.parse(localStorage.getItem("hoisting"))
 document.querySelector("#hoisting").addEventListener("click", myhoisting)
 function myhoisting(){
         document.querySelector("#container").innerText=null
-
-    hoistingval.map(function(elem,index){
         var div=document.createElement("ul")
-        var p1 = document.createElement("li")
-        p1.innerText=elem.p1
-        var p2 = document.createElement("li")
-        p2.innerText=elem.p2
-        var p3 = document.createElement("li")
-        p3.innerText=elem.p3
-        var p4 = document.createElement("li")
-        p4.innerText=elem.p4
-        var p5 = document.createElement("li")
-        p5.innerText=elem.p5
-       
-        div.append(p1,p2,p3,p4,p5)
+        for(var key in hoistingval)
+        {
+
+            var p1 = document.createElement("li")
+            p1.innerText= hoistingval[key]
+            
+            
+            div.append(p1)
+        }
         document.querySelector("#container").append(div)
-        
-    })
 
 
 }
