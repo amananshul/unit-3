4daf832a9a2a9f3fbda1cef4827502811b19dc96
amananshul <80920301+amananshul@users.noreply.github.com>
2022-03-21 15:03:34 +0530

    done

---
+++ cdP/component/comp.js
@@ -0,0 +1,11 @@
+const nav = ()=>{
+return`<div id="container">
+<h2>HOme</h2>
+<div>
+    <a href="jewel.html">jewellwey</a>
+    <a href="vaishali.html">vaishali</a>
+    <a href="elect.html">elect</a>
+</div>
+</div>`
+}
+export default nav
---
+++ cdP/component/jewel.js
@@ -0,0 +1,31 @@
+const getData=async(url)=>{
+    try{
+        
+        let res=await fetch (url);
+        let data= await res.json();
+        return data
+
+    }catch(e){
+        console.log(e)
+    }
+}
+const append=(data,parent)=>{
+    parent.innerHTML=null;
+data.map(({image,title,price})=>{
+
+    var div=document.createElement('div')
+
+    var img=document.createElement('img')
+    img.src=image;
+
+    var t=document.createElement('p')
+    t.innerText=title;
+    var p=document.createElement('p')
+    p.innerText=price;
+    div.append(img,t,p)
+    parent.append(div)
+})
+
+}
+
+export{getData,append}
