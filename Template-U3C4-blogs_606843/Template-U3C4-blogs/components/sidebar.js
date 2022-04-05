f8f32839e6aabb76370455b680f29a66077431bc
amananshul <80920301+amananshul@users.noreply.github.com>
2022-04-05 22:06:35 +0530

    done

---
+++ Template-U3C4-blogs_606843/Template-U3C4-blogs/components/sidebar.js
@@ -0,0 +1,11 @@
+function sidebar() {
+  // return your html component here
+  //Make sure to give input search box id as "searchbar"
+  return` <h2>Blog Search</h2>  
+  <input type="text" placeholder="Search" id="searchbar" >
+  <h4>Startups</h4>
+  <h4>blog letters</h4>
+  <h4>Audio</h4>
+  <h4>Video</h4>`
+}
+export default sidebar;
